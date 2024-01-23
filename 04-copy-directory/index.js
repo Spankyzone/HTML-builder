759b3a8724bdf5a3892d176ae1fe98ad4a98edc4
Aliaksandr Vel <dark1797@gmail.com>
2024-01-23 19:54:56 +0300

    fix: rewrite 04 task ater review

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -3,20 +3,27 @@ const fs = require('fs/promises');
 const src = path.join(__dirname, 'files');
 const dir = path.join(__dirname, 'files-copy');
 
-function copyDir() {
-  fs.mkdir(path.join(__dirname, 'files-copy'), { recursive: true }, (err) => {
-    console.log(err);
-  });
-  fs.readdir(src).then((files) => {
-    files.forEach((el) => {
-      fs.copyFile(src + `/${el}`, dir + `/${el}`)
-        .then(() => {
-          console.log('copy done');
-        })
-        .catch((err) => {
-          console.log(err);
-        });
-    });
-  });
+async function copyFiles() {
+  try {
+    await fs.access(src);
+    await fs.mkdir(dir, { recursive: true });
+    const files = await fs.readdir(src);
+    for (const file of files) {
+      const sourceFile = path.join(src, file);
+      const destinationFile = path.join(dir, file);
+      await fs.copyFile(sourceFile, destinationFile);
+    }
+    console.log('All files successfully copied');
+
+    const copiedFiles = await fs.readdir(dir);
+    for (const copiedFile of copiedFiles) {
+      if (!files.includes(copiedFile)) {
+        const fileToDelete = path.join(dir, copiedFile);
+        await fs.unlink(fileToDelete);
+      }
+    }
+  } catch (err) {
+    console.error('Error file copied:', err);
+  }
 }
-copyDir();
+copyFiles();
